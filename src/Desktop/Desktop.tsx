67bfecafc846dbd13fd164d3f3defeac51e2e2c6
Sebastian <sebastiangremm97@gmail.com>
2022-03-20 18:02:33 +0100

    Add desktop component

---
+++ src/Desktop/Desktop.tsx
@@ -0,0 +1,10 @@
+import { Nav } from "./Nav"
+import './Desktop.scss'
+
+export const Desktop = () => {
+    return (
+        <div className="desktop">
+            <Nav currentWindow="Note" />
+        </div>
+    )
+}
