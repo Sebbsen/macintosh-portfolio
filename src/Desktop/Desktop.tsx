d7313bf0ee5f9daef5a2c7bc89dd9e82e1e89833
Sebastian <sebastiangremm97@gmail.com>
2022-03-20 19:14:33 +0100

    Add window frame component

--- src/Desktop/Desktop.tsx
+++ src/Desktop/Desktop.tsx
@@ -1,10 +1,12 @@
 import { Nav } from "./Nav"
 import './Desktop.scss'
+import { WindowFrame } from "./Window/WindowFrame"
 
 export const Desktop = () => {
     return (
         <div className="desktop">
             <Nav currentWindow="Note" />
+            <WindowFrame title="Note - Todos" content="Lorem Ipsum dolor Santis est" active={true} />
         </div>
     )
 }
---
+++ src/Desktop/Window/WindowFrame.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import './WindowFrame.scss'
+
+type WindowFrameProps = {
+    active: boolean,
+    title: string,
+    content: string
+}
+
+export const WindowFrame = ({ active, title, content }: WindowFrameProps) => {
+
+    const [varName, setVarName] = useState('defaultValue');
+    
+    const style = {
+        width: '184px',
+        transform: 'translate(62px, 27px)'
+    };
+    
+    
+    useEffect(() => {
+        //do smth when prop changed
+    }, [title])
+
+
+    return (
+        <section className={"window-frame " + (active ? '' : 'window-frame--inactive')} style={style}>
+            <header className="window-frame__header">
+                <button className="button window-frame__btn"></button>
+                <h2 className="headline window-frame__title">{title}</h2>
+                <button className="button window-frame__btn window-frame__btn--size"></button>
+            </header>
+            <main className="window-frame__main">
+                {content}
+            </main>
+        </section>
+    )
+}
