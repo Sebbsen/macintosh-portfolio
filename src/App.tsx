1e088b96cc7052b1903301144d5c75ea97d43dfe
Sebastian <sebastiangremm97@gmail.com>
2022-03-21 14:04:31 +0100

    Add bg-image and overlays

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 import './App.scss'
+import bgMac from './images/bg-mac.webp';
+import bgMacShadow from './images/shadow.webp';
+import bgMacRefl from './images/refl.webp';
 import { Desktop } from './Desktop/Desktop'
 
 function App() {
   const [count, setCount] = useState(0)
+  const style = {
+    backgroundImage: `url(${bgMac})`,
+  }
 
   return (
-    <div className="App">
+    <div className="app" style={style}>
+      <img className="screen-overlay screen-overlay--refl" src={bgMacRefl} alt="" />
+      <img className="screen-overlay screen-overlay--shadow" src={bgMacShadow} alt="" />
       <Desktop />
     </div>
   )
