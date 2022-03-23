37389dc58f9f775143b8736386e4eba72d3b418e
Sebastian <sebastiangremm97@gmail.com>
2022-03-23 01:37:28 +0100

    Fix window layer system

--- src/Desktop/Desktop.tsx
+++ src/Desktop/Desktop.tsx
@@ -18,15 +18,37 @@ export const Desktop = () => {
             content: 'Lorem Ipsum dolor Santis est',
             defaultPosition: {x:31, y: 24},
             width: '190px',
-            zIndex: 2, 
+            zIndex: 4,
+            isHidden: false,
+        },
+        {
+            type: 'note',
+            id: 1,
+            title:'Note - Todos 2',
+            content: 'Lorem Ipsum dolor Santis est',
+            defaultPosition: {x:51, y: 44},
+            width: '190px',
+            zIndex: 3,
+            isHidden: false,
+        },
+        {
+            type: 'note',
+            id: 2,
+            title:'Note - Todos 3',
+            content: 'Lorem Ipsum dolor Santis est',
+            defaultPosition: {x:71, y: 64},
+            width: '190px',
+            zIndex: 2,
+            isHidden: false,
         },
         {
             type: 'about',
-            id: 1,
+            id: 3,
             title:'About Me',
             defaultPosition: {x:95, y: 95},
             width: '375px',
-            zIndex: 1, 
+            zIndex: 1,
+            isHidden: false,
         }
     ]
 
@@ -34,13 +56,16 @@ export const Desktop = () => {
 
     const handleActiveWindow = (id: number) => {
         let windowArray = [...windowsState];
+        let clickedWindowZIndex = windowArray[windowArray.findIndex((singleWindow => singleWindow.id == id))].zIndex;
         windowArray = windowArray.map(singleWindow => {
             let newZIndex;
 
             if (singleWindow.id === id) {
                 newZIndex = windowsState.length;  
-            } else {
+            } else if ( singleWindow.zIndex > clickedWindowZIndex) {
                 newZIndex = singleWindow.zIndex !== 1 ? singleWindow.zIndex - 1 : 1;
+            } else {
+                newZIndex = singleWindow.zIndex
             }
 
             singleWindow.zIndex = newZIndex;
